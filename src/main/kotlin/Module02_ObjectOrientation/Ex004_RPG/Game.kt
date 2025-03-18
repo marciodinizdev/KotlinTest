25c57defa9f36acac8846bc9473c5bb9960e94b5
Márcio Diniz <marciodiniz.dev@gmail.com>
2025-03-17 21:24:52 -0300

    Updates

---
+++ src/main/kotlin/Module02_ObjectOrientation/Ex004_RPG/Game.kt
@@ -0,0 +1,43 @@
+package Module02_ObjectOrientation.Ex004_RPG
+
+fun gameStart() {
+
+    // Characters
+    val noctis = Character(name = "Noctis", hp = 100, atk = 30, def = 10)
+
+    // Enemies
+    val enemies = listOf(
+        Enemy(name = "enemy", hp = 200, atk = 30, def = 10, exp = 100),
+        Enemy(name = "Cactuar", hp = 150, atk = 25, def = 15, exp = 50),
+        Enemy(name = "Tonberry", hp = 300, atk = 15, def = 10, exp = 200)
+    )
+    
+    // Items
+    val item = Item(name = "Small Potion", type = "potion", value = 100)
+    
+    // Start battle
+    val enemy = enemies.random()
+    
+    println("Battle starts! Enemy: ${enemy.name} | HP: ${enemy.hp}")
+    println("${noctis.name} HP: ${noctis.hp} --|x|-- ${enemy.name}  HP: ${enemy.hp} \n")
+    println("Choose an action: \n1. Attack \n2. Heal \n3. Run away")
+
+    while (noctis.alive() && enemy.alive()) {
+
+        println("Action: ")
+        var choice = readLine()
+        when (choice) {
+            "1" -> noctis.attack(enemy)
+            "2" -> item.use(noctis)
+            "3" -> {
+                println("You ran away!")
+                return
+            }
+            else -> println("Invalid choice")
+        }
+        if (enemy.hp > 0) {
+            enemy.attack(noctis)
+        }
+
+    }
+}
--- src/main/kotlin/Module02_ObjectOrientation/Ex004_RPG/Main.kt
+++ src/main/kotlin/Module02_ObjectOrientation/Ex004_RPG/Main.kt
@@ -1,30 +1,5 @@
 package Module02_ObjectOrientation.Ex004_RPG
 
 fun main () {
-
-    val noctis = Character(name = "Noctis", hp = 100, atk = 30, def = 10)
-    val behemoth = Enemy("Behemoth", 150, 30, 10, 150)
-    val item = Item(name = "Small Potion", type = "potion", value = 100)
-    println("${noctis.name} HP: ${noctis.hp} --|x|-- ${behemoth.name}  HP: ${behemoth.hp} \n")
-    println("Choose an action: \n1. Attack \n2. Heal \n3. Run away")
-
-    while (noctis.alive() && behemoth.alive()) {
-
-        println("Action: ")
-        var choice = readLine()
-        when (choice) {
-            "1" -> noctis.attack(behemoth)
-            "2" -> item.use(noctis)
-            "3" -> {
-                println("You ran away!")
-                return
-            }
-            else -> println("Invalid choice")
-        }
-        if (behemoth.hp > 0) {
-            behemoth.attack(noctis)
-        }
-
-    }
-
+    gameStart()
 }
